3402cb6a39acec5b18d8087f1c102629c6ba6dc9
umutarsln <umutarsln0@gmail.com>
2026-03-02 11:20:48 +0300

    domain maule cors girişi

--- main.py
+++ main.py
@@ -48,6 +48,8 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://127.0.0.1:3000",
+        "https://www.optiroll.pro",
+        "https://optiroll.pro",
         "https://3d-web-iduh-4os6uvukk-umuts-projects-ef16418a.vercel.app",
     ],
     allow_credentials=True,
